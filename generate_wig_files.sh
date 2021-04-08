ea8c9970a45b5ab2750adf486b1ecdc71ab639ab
Muntadher Jihad <muntadher.jihad@ideateknoloji.com.tr>
2021-04-08 14:37:42 +0300

    ok

---
+++ generate_wig_files.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sample = "/path/to/.bam"
+ref= "/path/to/hg19.fa" #make sure your bam is sorted (e.g. by samtools sort)
+map= "/path/to/wgEncodeCrgMapabilityAlign100mer.bigWig"
+
+./readCounter -w 10000 $sample > sample.wig
+./mapCounter -w 10000 $map > map.wig
+./mapCounter -w 10000 $ref > gc.wig
